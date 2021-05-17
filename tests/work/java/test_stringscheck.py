955209ed64c89f09b629f4b5060002640810119c
Jia <49358172+craftslab@users.noreply.github.com>
2021-05-17 21:02:15 +0800

    Add implementation for stringscheck test

--- tests/work/java/test_stringscheck.py
+++ tests/work/java/test_stringscheck.py
@@ -1,1 +1,28 @@
 # -*- coding: utf-8 -*-
+
+import os
+
+from lintwork.work.java.stringscheck import Stringscheck, StringscheckException
+
+
+def test_exception():
+    exception = StringscheckException("exception")
+    assert str(exception) == "exception"
+
+
+def test_stringscheck():
+    with open(
+        os.path.join(os.path.dirname(__file__), "../../data/java/stringscheck.txt"),
+        "r",
+    ) as f:
+        data = f.read()
+
+    try:
+        stringscheck = Stringscheck([])
+        buf = stringscheck._parse(data)
+    except StringscheckException as _:
+        assert False
+    else:
+        assert True
+
+    assert len(buf) != 0
