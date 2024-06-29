3585419cc8cf30b76a7cca1f1165c5fb666123a1
Jia Jia <angersax@sina.com>
2024-06-29 19:13:08 +0800

    refactor(lint): update proto files

--- script/proto.sh
+++ script/proto.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-src=lintwork/lint
+# Install protoc
+# curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-linux-x86_64.zip
 
+# Build proto
+src=lintwork/lint
 python -m grpc_tools.protoc -I./${src} --python_out=./${src} --grpc_python_out=./${src} ./${src}/lint.proto
 
 # Test
